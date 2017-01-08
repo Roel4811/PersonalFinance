3edaa6be97055dfa4c22be40d0dbf6a578709f5d
Roel de Jong <Roel4811@hotmail.com>
2017-01-08 22:27:30 +0100

    add form

---
+++ app/assets/javascripts/components/records.js.jsx
@@ -0,0 +1,39 @@
+var Records = React.createClass({
+  getInitialState: function() {
+    return { records: this.props.data };
+  },
+
+  getDefaultProps: function() {
+    return { records: [] };
+  },
+
+  addRecord: function(record) {
+    var records = React.addons.update(this.state.records, { $push: [record] })
+    this.setState({ records: records });
+  },
+
+  render: function () {
+    return(
+      <div className='records'>
+        <h2 className='title'>
+          Records
+        </h2>
+        <RecordForm handleNewRecord={this.addRecord} />
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.records.map(function(record) {
+              return <Record key={record.id} record={record} />
+             }.bind(this))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
